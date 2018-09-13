9cd7ed6367590d0d39b67fcceba00525b1d07a7b
Emmanuel Cossard <emmanuel.cossard@etudiant.univ-lr.fr>
2018-09-13 15:27:44 +0200

    correction class repos

--- PingPongApp/src/pingpongapp/etat/Repos.java
+++ PingPongApp/src/pingpongapp/etat/Repos.java
@@ -35,85 +35,94 @@ public class Repos implements Etat{
     public void init() {
        if(joueur instanceof Client)
        {
-           //envoie de la demande de partie au serveur
-           joueur.lancerConnexion();
-           LOG.log(Level.INFO,"je suis un client j'envoi une demande de partie");
-           Init init=null;
-           init=new Init(11);
-           try {
-               
-              joueur.getOutput().writeObject(init);
-              LOG.log(Level.INFO,"envoie");
-           }catch(Exception e)
-            {
-        	   LOG.log(Level.SEVERE, e.getMessage(), e);
-            }
-           joueur.setEtat(joueur.getEtatInitActive());
+    	   jeSuisJoueur();
          
        }
        if(joueur instanceof Serveur)
        { 
-    	   LOG.log(Level.INFO,"je suis un serveur à l'état de repos");
-           joueur.lancerConnexion();
-            Init objet=null;
-           //attente de demande
-            try
-            {
-            	 LOG.log(Level.INFO,"Attente de la reception d'une demande de partie");
-                   objet=(Init) joueur.getInput().readObject(); 
-                   LOG.log(Level.INFO,"j ai recus une demande : ");
-                   LOG.log(Level.INFO,"{0}",objet);
-            }
-             catch(Exception e)
-                 {
-            	 LOG.log(Level.SEVERE, e.getMessage(), e);
-                 }
-            
-            //envoie de la reponse
-              Ackinit reponse=null;
-            if(joueur.getScoreMax()==objet.getScoreMax())
-            {
-                reponse=new Ackinit("oui");
-                joueur.setEtat(joueur.getEtatJeSers());
-                try {
-               
-              joueur.getOutput().writeObject(reponse);
-              LOG.log(Level.INFO,"envoie de la réponse");
-           }catch(Exception e)
-            {
-        	   LOG.log(Level.SEVERE, e.getMessage(), e);
-            }
-            }
-            else
-            {
-              
-                reponse=new Ackinit("non");
-                LOG.log(Level.INFO," ===== je refuse la partie ===== ");
-                LOG.log(Level.INFO,"{0}",objet);
-                try 
-                {
-                    joueur.getOutput().writeObject(reponse);
-                    LOG.log(Level.INFO,"envoie de la réponse");
-                }catch(Exception e)
-                {
-                	LOG.log(Level.SEVERE, e.getMessage(), e);
-                }
-                try 
-                {
-                    joueur.getInput().close();
-                    joueur.getOutput().close();  
-                   
-                } catch(IOException e) 
-                {
-                	LOG.log(Level.SEVERE, e.getMessage(), e);
-                    return;
-                }   
-                
-            }
+    	  jeSuisServeur();
        }
         
     }
-
+    
+    private void jeSuisJoueur()
+    {
+    	//envoie de la demande de partie au serveur
+        joueur.lancerConnexion();
+        LOG.log(Level.INFO,"je suis un client j'envoi une demande de partie");
+        Init init=null;
+        init=new Init(11);
+        try {
+            
+           joueur.getOutput().writeObject(init);
+           LOG.log(Level.INFO,"envoie");
+        }catch(Exception e)
+         {
+     	   LOG.log(Level.SEVERE, e.getMessage(), e);
+         }
+        joueur.setEtat(joueur.getEtatInitActive());
+    }
+    
+    private void jeSuisServeur()
+    {
+    	 LOG.log(Level.INFO,"je suis un serveur à l'état de repos");
+         joueur.lancerConnexion();
+          Init objet=null;
+         //attente de demande
+          try
+          {
+          	 LOG.log(Level.INFO,"Attente de la reception d'une demande de partie");
+                 objet=(Init) joueur.getInput().readObject(); 
+                 LOG.log(Level.INFO,"j ai recus une demande : ");
+                 LOG.log(Level.INFO,"{0}",objet);
+          }
+           catch(Exception e)
+               {
+          	 LOG.log(Level.SEVERE, e.getMessage(), e);
+               }
+          
+          //envoie de la reponse
+            Ackinit reponse=null;
+          if(joueur.getScoreMax()==objet.getScoreMax())
+          {
+              reponse=new Ackinit("oui");
+              joueur.setEtat(joueur.getEtatJeSers());
+              try {
+             
+            joueur.getOutput().writeObject(reponse);
+            LOG.log(Level.INFO,"envoie de la réponse");
+         }catch(Exception e)
+          {
+      	   LOG.log(Level.SEVERE, e.getMessage(), e);
+          }
+          }
+          else
+          {
+            
+              reponse=new Ackinit("non");
+              LOG.log(Level.INFO," ===== je refuse la partie ===== ");
+              LOG.log(Level.INFO,"{0}",objet);
+              try 
+              {
+                  joueur.getOutput().writeObject(reponse);
+                  LOG.log(Level.INFO,"envoie de la réponse");
+              }catch(Exception e)
+              {
+              	LOG.log(Level.SEVERE, e.getMessage(), e);
+              }
+              try 
+              {
+                  joueur.getInput().close();
+                  joueur.getOutput().close();  
+                 
+              } catch(IOException e) 
+              {
+              	LOG.log(Level.SEVERE, e.getMessage(), e);
+                  return;
+              }   
+              
+          }	
+    }
     @Override
     public void attenteAck() {
         throw new UnsupportedOperationException(NOT_SUPPORTED);
